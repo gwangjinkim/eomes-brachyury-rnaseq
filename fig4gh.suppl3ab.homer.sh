dfe20c795357ad8b0094cdf96eac54b511edf797
Josephus Kim <gwangjin_kim@yahoo.com>
2019-10-09 10:31:52 +0200

    corrected

---
+++ fig4gh.suppl3ab.homer.sh
@@ -0,0 +1,48 @@
+####################################
+# analysis for Fig 4 g+h using HOMER
+####################################
+
+
+wt_atac_peaks="/media/josephus/Elements/galaxy_atac_data/atac-wt-merge-86.bed"
+dko_atac_peaks="/media/josephus/Elements/galaxy_atac_data/atac-dko-merge-92.bed"
+
+mkdir -p ${wt_atac_out}
+mkdir -p ${dko_atac_out}
+
+findMotifsGenome.pl ${wt_atac_peaks} mm10 ${wt_atac_out}/ -bg ${dko_atac_peaks} -size 200 -p 6 && \
+findMotifsGenome.pl ${dko_atac_peaks} mm10 ${dko_atac_out} -bg ${wt_atac_peaks} -size 200 -p 6
+
+rsync -av /media/josephus/Elements/ChIPresults/ /media/josephus/24AA554AAA551A1E/ChIPresults/
+
+
+################################################################################
+
+####################################
+# analysis for Suppl Fig 3 a+b
+####################################
+
+path_to_tosicj190315="/media/josephus/My Book/1TB_backup/tosicj190315"
+out="/media/josephus/Elements/ChIPresults/homer/ChIPseq2019"
+mkdir -p ${out}
+
+eo_final_peak_fpath="${path_to_tosicj190315}/Peaks_ChIP_EoGFP_pileup120.bed"
+br_final_peak_fpath="${path_to_tosicj190315}/Peaks_ChIP_BraGFP_pileup40.bed"
+
+out_eo_bg_br="${out_eo}/eo_final_bg_br_final"
+out_br_bg_eo="${out_br}/br_final_bg_eo_final"
+
+mkdir -p ${out_eo_bg_br}
+mkdir -p ${out_br_bg_eo}
+
+findMotifsGenome.pl "${eo_final_peak_fpath}" mm10 "${out_eo_bg_br}"/ -bg "${br_final_peak_fpath}" -size 200 -p 6 && \
+findMotifsGenome.pl "${br_final_peak_fpath}" mm10 "${out_br_bg_eo}"/ -bg "${eo_final_peak_fpath}" -size 200 -p 6
+
+
+
+
+
+
+
+
+
+
